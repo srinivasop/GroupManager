fec120d81ff4ff315bf0c90a6a8f0eb4693982bb
srinivasop <73180137+srinivasop@users.noreply.github.com>
2020-11-01 09:59:31 +0530

    Update __main__.py

--- haruka/__main__.py
+++ haruka/__main__.py
@@ -29,11 +29,10 @@ I'm group manager bot and Anti-spam for RR Players🃏
 Activate Anti-spam protection in your BM by this command : 
 /antispam on 
 
-Made from the source code of [Aditya 🇮🇳](t.me/xditya)
+i am a bot made by srinivas and i can help you add me to your supergroup
 
-Special thanks to [this person](t.me/denver02) for helping me.
+and a special thanks to pranavjha he helped me a lot 
 
-Deploy a bot like me [now](www.github.com/xditya/GroupManager)
 
 Watch [video tutorial](https://www.youtube.com/watch?v=gXXFpTAk6Vo&feature=youtu.be) on deploying me.
 
